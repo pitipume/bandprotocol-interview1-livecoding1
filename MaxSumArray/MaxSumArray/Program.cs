f58b8056d3c53b2b0a524e3ce81513996e3e7c34
pitipume <pitipume.boonyawan@gmail.com>
2024-06-10 22:12:40 +0700

    fix: adjust main logic

--- MaxSumArray/MaxSumArray/Program.cs
+++ MaxSumArray/MaxSumArray/Program.cs
@@ -2,43 +2,27 @@
 {
     public static void Main(string[] args)
     {
-        int[] array = { 5, 4, 19999, 99, 77 };
-        int k = 3;
-        int i = 0;
-        int j = 1;
-        int l = 2;
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int k = 3; // sum of K number consecutive
+        var n = array.Length - k; // number of n loop
         var sum1 = 0;
         var sum2 = 0;
 
-        for (i = 0; i <= 3; i++)
+        for (int i = 0; i <= n; i++)
         {
-            if (i <= array.Length || l <= array.Length || j <= array.Length)
+            Console.Write($"Round {i + 1}: ");
+            Console.Write("{0}+{1}+{2} = ", array[i], array[i + 1], array[i + 2]);
+            if (i <= array.Length)
             {
-                sum1 = array[i] + array[j] + array[l];
-
-            }
-
-            if (sum2 > sum1)
-            {
-                Console.WriteLine(sum2);
-
-            }
-            else
-            {
+                sum1 = array[i] + array[i + 1] + array[i + 2];
                 Console.WriteLine(sum1);
             }
-            sum2 = sum1;
-            i++;
-            j++;
-            l++;
+
+            if (sum1 > sum2)
+            {
+                sum2 = sum1;
+            }
         }
-        if (sum2 > sum1)
-        {
-            Console.WriteLine($"Max sum: {sum2}");
-        }
-        else
-        {
-            Console.WriteLine($"Max sum: {sum1}");
-        }
+        Console.WriteLine($"MaxSum: {sum2}");
     }
 }
